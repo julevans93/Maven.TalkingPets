912e10a90800ba75f694360e3688a420d2ae5ed4
Miral Doshi <miraldoshi@zipcodersMBP41.localdomain>
2019-11-15 11:27:57 -0500

    add Cat

---
+++ src/main/java/io/zipcoder/polymorphism/Cat.java
@@ -0,0 +1,22 @@
+package io.zipcoder.polymorphism;
+
+public class Cat extends Pet {
+        private   String name;
+        private String speak;
+
+        public Cat(String speak, String name){
+            super(speak,name);
+        }
+
+        public String speak(){
+            return "Bark";
+        }
+
+        public void setName(){
+            this.name= name;
+        }
+
+        public String getName(){
+            return this.name;
+        }
+}
