d7094e6601e654a9c920373718b1761b98d51c7f
Julia Evans <julevans93@users.noreply.github.com>
2019-11-15 11:17:42 -0500

    created Pet and animal class

---
+++ src/main/java/io/zipcoder/polymorphism/Bird.java
@@ -0,0 +1,13 @@
+package io.zipcoder.polymorphism;
+
+public class Bird extends Pet {
+
+
+    public Bird(String speak, String name) {
+        super(speak, name);
+    }
+
+    public String speak(){
+        return "tweet!";
+    }
+}
